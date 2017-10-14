334671d32564ca7cd144dc06f097732498d44d0c
innkeeper <jerrytang513@hotmail.com>
2017-10-13 23:03:37 -0400

    Updated Navigation Bar and UI

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -1,7 +1,11 @@
 from django.conf.urls import url
 
 from . import views
+app_name = 'portfolio'
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^about/', views.about, name='about'),
+    url(r'^project/', views.project, name='project'),
+    url(r'contact/', views.contact, name='contact'),
 ]
--- portfolio/views.py
+++ portfolio/views.py
@@ -4,3 +4,12 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'portfolio/index.html')
+
+def about(request):
+    return render(request, 'portfolio/about.html')
+
+def project(request):
+    return render(request, 'portfolio/project.html')
+
+def contact(request):
+    return render(request, 'portfolio/contact.html')
