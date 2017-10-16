8c785bca2dbb09580336a495a642eaffdaeae192
innkeeper <jerrytang513@hotmail.com>
2017-10-16 06:57:55 -0400

    Added A video animation

--- portfolio/static/portfolio/js/slider.js
+++ portfolio/static/portfolio/js/slider.js
@@ -1,17 +1,27 @@
-var pos = 0;
+/*var pos = 0;
+var slides = document.getElementsByClassName("slide");
+var contents = document.getElementsByClassName("inline");
 
 function autoSlide(){
   var index;
-  var slides = document.getElementsByClassName("slide");
+
   for(index = 0; index < slides.length; index++){
     slides[index].style.display = "none";
+  }
+  for(index = 0; index < contents.length; index++){
+    contents[index].style.display = "none";
   }
   if(pos >= slides.length){
     pos = 0;
   }
   slides[pos].style.display = "block";
+  for(index = 0; index < 3; index ++){
+    contents[pos * 3 + index].style.display = "block";
+  }
   pos ++;
-  setTimeout(autoSlide,4000);
+  setTimeout(autoSlide,2500);
+
 }
 
 autoSlide();
+*/
