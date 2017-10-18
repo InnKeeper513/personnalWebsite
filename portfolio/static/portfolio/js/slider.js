55596b3c65d6ad18acb3a323aecf16f9280d2cbf
innkeeper <jerrytang513@hotmail.com>
2017-10-18 07:09:20 -0400

    Updated Button and Messages

--- portfolio/static/portfolio/js/slider.js
+++ portfolio/static/portfolio/js/slider.js
@@ -1,27 +1,50 @@
-/*var pos = 0;
-var slides = document.getElementsByClassName("slide");
-var contents = document.getElementsByClassName("inline");
+'use strict';
 
-function autoSlide(){
-  var index;
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-  for(index = 0; index < slides.length; index++){
-    slides[index].style.display = "none";
-  }
-  for(index = 0; index < contents.length; index++){
-    contents[index].style.display = "none";
-  }
-  if(pos >= slides.length){
-    pos = 0;
-  }
-  slides[pos].style.display = "block";
-  for(index = 0; index < 3; index ++){
-    contents[pos * 3 + index].style.display = "block";
-  }
-  pos ++;
-  setTimeout(autoSlide,2500);
+var Clock = function () {
+  function Clock(id) {
+    _classCallCheck(this, Clock);
 
-}
+    var el = document.getElementById(id);
+    this.handHr = el.querySelector('.clock__hand--hour');
+    this.handMin = el.querySelector('.clock__hand--minute');
+    this.handSec = el.querySelector('.clock__hand--second');
 
-autoSlide();
-*/
+    this.setTime();
+    this.init();
+  }
+
+  Clock.prototype.tick = function tick(hrs, mins, secs) {
+    var degHrs = hrs / 12 * 360 + 90;
+    var degMins = mins / 60 * 360 + 90;
+    var degSecs = secs / 60 * 360 + 90;
+
+    this.handHr.style.transform = 'rotate(' + degHrs + 'deg)';
+    this.handMin.style.transform = 'rotate(' + degMins + 'deg)';
+    this.handSec.style.transform = 'rotate(' + degSecs + 'deg)';
+
+    if (degSecs === 90) {
+      this.handSec.style.transitionDuration = "0s";
+    } else {
+      this.handSec.style.transitionDuration = "0.1s";
+    }
+  };
+
+  Clock.prototype.setTime = function setTime() {
+    var now = new Date();
+    var hrs = now.getHours();
+    var mins = now.getMinutes();
+    var secs = now.getSeconds();
+
+    this.tick(hrs, mins, secs);
+  };
+
+  Clock.prototype.init = function init() {
+    setInterval(this.setTime.bind(this), 1000);
+  };
+
+  return Clock;
+}();
+
+var clock = new Clock("new-clock");
