dbf86de4c748eaa34b2d698da68ecbbdd6aefb9a
innkeeper <jerrytang513@hotmail.com>
2017-11-05 18:49:55 -0500

    Able to reset password

--- jerry_portfolio/urls.py
+++ jerry_portfolio/urls.py
@@ -16,7 +16,16 @@ Including another URLconf
 from django.conf.urls import url,include
 from django.contrib import admin
 from portfolio import views as port_view
+from django.contrib.auth.views import (
+    password_reset, password_reset_done, password_reset_confirm, password_reset_complete
+)
+
 urlpatterns = [
+    url(r'^reset-password/$', password_reset, name='reset_password'),
+    url(r'^reset-password/done/$', password_reset_done, name='password_reset_done'),
+    url(r'^reset-password/confirm/(?P<uidb64>[0-9A-Za-z]+)-(?P<token>.+)/$',
+        password_reset_confirm, name='password_reset_confirm'),
+    url(r'^reset-password/complete/$', password_reset_complete, name='password_reset_complete'),
     url(r'', include('portfolio.urls', namespace='portfolio')),
     url(r'^admin/', admin.site.urls),
     url(r'localjava/',include('localjava.urls',namespace='localjava')),
